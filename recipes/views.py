ab412adc8b65820b47a5d9a8a00421057d961dd2
macpinheiro <daniel.pinheiro.franco@gmail.com>
2023-12-09 21:45:45 -0300

    Refactor HTML templates and views

--- recipes/views.py
+++ recipes/views.py
@@ -5,8 +5,3 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/home.html')
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
